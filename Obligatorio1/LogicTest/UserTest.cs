3afde50282dea0361e6f61ca57888aa1473df699
Marcel Cohen <diegomarcel27@hotmail.com>
2018-08-31 20:45:36 -0300

    se agrega proyecto test con pruebas para la clase user

---
+++ Obligatorio1/LogicTest/UserTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicTest
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void NickNameTest()
+        {
+            User fake = new User("nickname", "path");
+            Assert.AreEqual(fake.Name,"nickname");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUserDataException))]
+        public void EmptyUserNameTest() {
+            User fake = new User("", "path");
+
+        }
+
+        [TestMethod]
+        public void PathTest() {
+            User fake = new User("nickname", "path");
+            Assert.AreEqual(fake.Path, "path");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUserDataException))]
+        public void InvalidPathTest() {
+            User fake = new User("nickname", "");
+        }
+
+        [TestMethod]
+        public void EqualsTest() {
+            User fake1 = new User("nickname", "path1");
+            User fake2 = new User("nickname", "path2");
+            Assert.AreEqual(fake1, fake2);
+        }
+
+        [TestMethod]
+        public void NotEqualsDifferentNicknamesTest() {
+            User fake1 = new User("nickname", "path1");
+            User fake2 = new User("nicknameee", "path2");
+            Assert.AreNotEqual(fake1, fake2);
+        }
+
+        [TestMethod]
+        public void NotEqualsOtherTypeTest() {
+            User fake1 = new User("nickname", "path1");
+            Assert.IsFalse(fake1.Equals(new object()));
+        }
+
+        [TestMethod]
+        public void NotEqualsNullTest() {
+            User fake1 = new User("nickname", "path1");
+            Assert.IsFalse(fake1.Equals(null));
+        }
+    }
+}
