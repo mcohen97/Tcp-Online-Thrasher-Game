b9c7df4073ae76285e27149d302a9d187432f5eb
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-09-02 12:41:22 -0300

    Se crea el paquete de pruebas y logica del juego

---
+++ Obligatorio1/GameLogicTest/SurvivorTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace GameLogicTest
+{
+    [TestClass]
+    public class SurvivorTest
+    {
+        [TestMethod]
+        public void NewSurvivorTest()
+        {
+            
+            Survivor survivor = new Survivor()
+        }
+    }
+}
