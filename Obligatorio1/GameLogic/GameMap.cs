91c5f915aba1c02bbcfd624bc29425abed06fbf7
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-09-07 19:52:26 -0300

    Pruebas verdes y nuevas pruebas agregadas

--- Obligatorio1/GameLogic/GameMap.cs
+++ Obligatorio1/GameLogic/GameMap.cs
@@ -1,9 +1,105 @@
-﻿namespace GameLogicTest
+﻿using System;
+using GameLogic;
+using GameLogicException;
+
+namespace GameLogicTest
 {
-    internal class GameMap
+    public class GameMap
     {
+        private int height;
+        private int length;
+        private Player[,] map;
+        private int playerCount;
+
+        public int PlayerCount {
+            get {
+                return playerCount;
+            }
+            private set {
+                playerCount = value;
+            }
+        }
+
         public GameMap()
         {
+            this.length = 8;
+            this.height = 8;
+            this.map = new Player[length, height];
+            this.PlayerCount = 0;
+        }
+
+        public GameMap(int length, int height)
+        {
+            this.length = length;
+            this.height = height;
+            this.map = new Player[length, height];
+            this.PlayerCount = 0;
+        }
+
+        public void AddPlayerToPosition(Player player, Position initialPosition)
+        {
+            if (!IsPositionValid(initialPosition))
+                throw new InvalidPositionException();
+
+            if (!IsPositionEmpty(initialPosition))
+                throw new OccupiedPositionException();
+
+            map[initialPosition.Row, initialPosition.Column] = player;
+            player.Position = initialPosition;
+            playerCount++;
+            
+        }
+
+        public void MovePlayer(Position from, Position to)
+        {
+            if (!IsPositionEmpty(from))
+            {
+                if (!IsPositionValid(to))
+                    throw new InvalidPositionException();
+
+                if (!IsPositionEmpty(to))
+                    throw new OccupiedPositionException();
+
+                Player playerMoved = map[from.Row, from.Column];
+                map[to.Row, to.Column] = playerMoved;
+                playerMoved.Position = to;
+                map[from.Row, from.Column] = null;
+            }
+        }
+
+        public void RemovePlayer(Position position)
+        {
+            if (!IsPositionValid(position))
+                throw new InvalidPositionException();
+
+            map[position.Row, position.Column] = null;
+            playerCount--;
+        }
+
+        public Player GetPlayer(Position position)
+        {
+            if (!IsPositionValid(position))
+                throw new InvalidPositionException();
+
+            return map[position.Row, position.Column];
+        }
+
+        public bool IsPositionEmpty(Position position)
+        {
+            if (!IsPositionValid(position)) return false;
+
+            return map[position.Row, position.Column] == null;
+        }
+
+        private bool IsPositionValid(Position position)
+        {
+
+            if (position.Row >= height || position.Row < 0)
+                return false;
+            if (position.Column >= length || position.Column < 0)
+                return false;
+
+            return true;
         }
     }
 }
--- Obligatorio1/GameLogicTest/GameMapTest.cs
+++ Obligatorio1/GameLogicTest/GameMapTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GameLogic;
+using GameLogicException;
 
 namespace GameLogicTest
 {
@@ -8,10 +9,154 @@ namespace GameLogicTest
     public class GameMapTest
     {
         [TestMethod]
-        public void NewGameMap()
+        public void NewGameMapTest()
         {
             GameMap gameMap = new GameMap();
             Assert.IsNotNull(gameMap);
+        }
+
+        [TestMethod]
+        public void ParametterConstructorTest()
+        {
+            int length = 8;
+            int height = 8;
+            GameMap gameMap = new GameMap(length, height);
+            Assert.IsNotNull(gameMap);
+        }
+
+        [TestMethod]
+        public void AddPlayerToPositionTest()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player = new Monster();
+            Position initialPosition = new Position(0, 0);
+            gameMap.AddPlayerToPosition(player, initialPosition);
+            Assert.AreEqual(player.Position, initialPosition);
+        }
+
+        [TestMethod]
+        public void MovePlayerTest()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player = new Monster();
+            Position initialPosition = new Position(0, 0);
+            gameMap.AddPlayerToPosition(player, initialPosition);
+            Position finalPosition = new Position(1, 1);
+            gameMap.MovePlayer(initialPosition, finalPosition);
+            Assert.AreEqual(player.Position, finalPosition);
+        }
+
+        [TestMethod]
+        public void IsPositionEmptyFalseTest()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player = new Monster();
+            Position position = new Position(0, 0);
+            gameMap.AddPlayerToPosition(player, position);
+            Assert.IsFalse(gameMap.IsPositionEmpty(position));
+        }
+
+        [TestMethod]
+        public void RemovePlayerTest()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player = new Monster();
+            Position position = new Position(0, 0);
+            gameMap.AddPlayerToPosition(player, position);
+            gameMap.RemovePlayer(player.Position);
+            Assert.IsTrue(gameMap.IsPositionEmpty(position));
+        }
+
+        [TestMethod]
+        public void PlayersInGameTest()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player1 = new Monster();
+            Player player2 = new Survivor();
+            Player player3 = new Survivor();
+
+            gameMap.AddPlayerToPosition(player1, new Position(0, 0));
+            gameMap.AddPlayerToPosition(player2, new Position(0, 1));
+            gameMap.AddPlayerToPosition(player3, new Position(0, 2));
+            gameMap.RemovePlayer(player3.Position);
+
+            Assert.AreEqual(2, gameMap.PlayerCount);
+        }
+
+        //Exceptions Tests
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPositionException))]
+        public void InvalidInitialPositionTest1()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player = new Monster();
+            gameMap.AddPlayerToPosition(player, new Position(-1, -1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPositionException))]
+        public void InvalidInitialPositionTest2()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player = new Monster();
+            gameMap.AddPlayerToPosition(player, new Position(10, 0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPositionException))]
+        public void InvalidInitialPositionTest3()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player = new Monster();
+            gameMap.AddPlayerToPosition(player, new Position(2, 10));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPositionException))]
+        public void InvalidMovePositionTest1()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player = new Monster();
+            gameMap.AddPlayerToPosition(player, new Position(0, 0));
+            gameMap.MovePlayer(player.Position, new Position(-1, 0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPositionException))]
+        public void InvalidMovePositionTest2()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player = new Monster();
+            gameMap.AddPlayerToPosition(player, new Position(0, 0));
+            gameMap.MovePlayer(player.Position, new Position(10, 0)); //GameMap doesnt have the responsability of control game rules
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPositionException))]
+        public void InvalidMovePositionTest3()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player player = new Monster();
+            gameMap.AddPlayerToPosition(player, new Position(0, 0));
+            gameMap.MovePlayer(player.Position, new Position(2, 10));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OccupiedPositionException))]
+        public void OccupiedPositionExceptionTest()
+        {
+            GameMap gameMap = new GameMap(8, 8);
+            Player playerMoved = new Monster();
+            Player playerInPosition = new Survivor();
+            Position initialPosition = new Position(0, 0);
+            Position finalPosition = new Position(0, 1);
+
+            gameMap.AddPlayerToPosition(playerMoved, new Position(0, 0));
+            gameMap.AddPlayerToPosition(playerInPosition, new Position(0, 1));
+
+            gameMap.MovePlayer(initialPosition, finalPosition);
+
         }
     }
 }
