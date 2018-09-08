578bee31a6a376d34c725061049024572f6d9f54
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-09-07 21:23:50 -0300

    Se agrega la interfaz IPlayerController

---
+++ Obligatorio1/GameLogic/PlayerControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    interface IPlayerController
+    {
+        void Move(Movement movement);
+        void Turn(Direction direction);
+    }
+}
