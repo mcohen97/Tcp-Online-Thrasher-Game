a7214981a03a70d7ee23f88d040f84476ad82f86
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-01 18:38:13 -0300

    se captura el evento de  cancelacion de consola para que realice la limpieza adecuadamente

--- Obligatorio1/ConsoleServer/Program.cs
+++ Obligatorio1/ConsoleServer/Program.cs
@@ -11,16 +11,42 @@ namespace ConsoleServer
 {
     class Program
     {
+        [System.Runtime.InteropServices.DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler _handler;
+
+        enum CtrlType
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+        public static Server gameServer;
+
+        private static bool Handler(CtrlType sig)
+        {
+            gameServer.serverWorking = false;
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            Server s = new Server();
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+
+            gameServer = new Server();
             Thread listen = new Thread(new ThreadStart(() =>
             {
-                s.ListenToRequests();
+                gameServer.ListenToRequests();
             }));
             listen.IsBackground = true;
             listen.Start();
-            s.ServerManagement();     
-        }
+            gameServer.ServerManagement();     
+        } 
     }
 }
