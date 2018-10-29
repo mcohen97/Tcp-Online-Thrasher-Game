216edb29a2e6d1fd16de4744370d85a18bee2014
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-29 13:01:43 -0300

    se crea proyecto de servidor administrativo

---
+++ Obligatorio1/AdministrativeServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdministrativeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
