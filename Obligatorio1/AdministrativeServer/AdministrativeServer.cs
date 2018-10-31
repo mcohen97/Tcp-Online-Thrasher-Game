24d4da4745b7fc7a5d330b89b52c4028c9b6657c
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-31 17:40:44 -0300

    se crea proyecto AdministrativeServer donde estara el servidor con las nuevas funcionalidades del sistema

---
+++ Obligatorio1/AdministrativeServer/AdministrativeServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UserABM;
+
+namespace AdministrativeServer
+{
+    public class AdministrativeServer
+    {
+        IUserCRUDService remoteUserStorage;
+
+        public AdministrativeServer() {
+            remoteUserStorage = (IUserCRUDService)Activator.GetObject(typeof(IUserCRUDService), "tcp://127.0.0.1:8000/Obligatorio2");
+        }
+
+        public void AddFakeUser() {
+            UserDto fake = new UserDto { nickname = "test", photoPath = "test" };
+            remoteUserStorage.AddUser(fake);
+        }
+    }
+}
