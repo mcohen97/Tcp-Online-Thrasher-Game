38074a120f4ca10e259304e8248b08163754b719
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-31 19:21:04 -0300

    se agrega Interfaz de repositorio de Scores, y su implementacion, como singleton

---
+++ Obligatorio1/DataAccess/IScoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using ScoreService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public interface IScoreRepository
+    {
+        void AddScore(Score aScore);
+
+        ICollection<Score> GetScores();
+    }
+}
---
+++ Obligatorio1/DataAccess/ScoresInMemory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ScoreService;
+
+namespace DataAccess
+{
+    public class ScoresInMemory: IScoreRepository
+    {
+        public static readonly Lazy<ScoresInMemory> instance = new Lazy<ScoresInMemory>(() => new ScoresInMemory(10));
+        private Score[] scoresQueue;
+        private int nLastScores;
+        private int frontPos;
+
+        public ScoresInMemory(int reelevantLastScores) {
+            nLastScores = reelevantLastScores;
+            scoresQueue = new Score[nLastScores];
+            frontPos = 0;
+        }
+
+        public void AddScore(Score aScore)
+        {
+            int nextPos = ++frontPos % nLastScores;
+            scoresQueue[nextPos] = aScore;
+        }
+
+        public ICollection<Score> GetScores()
+        {
+            ICollection<Score> sorted = new List<Score>();
+            int auxPos = frontPos;
+            for (int i = 0; i < scoresQueue.Length; i++) {
+                sorted.Add(scoresQueue[auxPos]);
+                //adding nLastScores transform position -1 into the last one (circularity)
+                auxPos = (nLastScores + (--auxPos)) % nLastScores;
+            }
+           return sorted.Reverse().ToList();
+        }
+    }
+}
