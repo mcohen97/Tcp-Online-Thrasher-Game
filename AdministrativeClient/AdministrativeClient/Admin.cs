018e87422cebcee3a0559260a2716a3a2ff06831
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-01 20:15:36 -0300

    se mueve el cliente administrativo a una solucion aparte, transitorio. Se corroboro correcto funcionamiento

---
+++ AdministrativeClient/AdministrativeClient/Admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AdministrativeClient.ServiceReference;
+
+namespace Client
+{
+    public class Admin
+    {
+        private IWebService server;
+        public Admin() {
+            server = new WebServiceClient();
+        }
+
+        public void AddUser() {
+            UserDto newUser = new UserDto() { nickname = "Richard", photoPath = "aPhoto" };
+            server.AddUser(newUser);
+        }
+
+        internal void GetUser()
+        {
+            UserDto retrieved = server.Get("Richard");
+            Console.WriteLine(retrieved.nickname + " " + retrieved.photoPath);
+            Console.ReadLine();
+        }
+    }
+}
