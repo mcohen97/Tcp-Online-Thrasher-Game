a183a1ceb0b6ee65ee352c74f62d7115f97cc9eb
Marcel Cohen <diegomarcel27@hotmail.com>
2018-10-31 22:49:14 -0300

    se agrega la implmentacion del servicio de scores que se expondra por remoting

---
+++ Obligatorio1/DataAccess/Properties/ScoreService.cs
@@ -0,0 +1,22 @@
+﻿using RemoteServicesContracts;
+using ScoreService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Properties
+{
+    public class ScoreService : MarshalByRefObject, IScoreService
+    {
+        private IScoreRepository scores;
+        public ScoreService() {
+            scores = ScoresInMemory.instance.Value;
+        }
+        public ICollection<Score> GetLastScores()
+        {
+            return scores.GetScores();
+        }
+    }
+}
