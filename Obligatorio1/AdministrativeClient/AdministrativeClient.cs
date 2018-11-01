851162a1efb13c103a8c1a487d714074675d2304
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-01 18:42:18 -0300

    se crea proyecto cliente administrativo

---
+++ Obligatorio1/AdministrativeClient/AdministrativeClient.cs
@@ -0,0 +1,16 @@
+﻿using AdministrativeClient.GameServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministrativeClient
+{
+    public class AdministrativeClient
+    {
+        public AdministrativeClient() {
+            //ServiceReference1.IWebService server = new ServiceReference1.WebServiceClient();
+        }
+    }
+}
