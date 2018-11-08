f660676a840d91d9a674ce9af93cef116daa9251
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-08 14:26:36 -0300

    se agrega funcionalidad de interaccion con el usuario en Admin

--- AdministrativeClient/AdministrativeClient/Admin.cs
+++ AdministrativeClient/AdministrativeClient/Admin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using AdministrativeClient.ServiceReference;
 
 namespace Client
@@ -13,15 +14,80 @@ namespace Client
         internal void RunClient()
         {
             bool endGame = false;
-            string[] options = new string[] {"Add user", "Modify user", "Delete user", "" }
+            string[] options = new string[] { "Add user", "Modify user", "Delete user",
+                "Get all users", "Get last match log", "Get top match scores" };
             while (!endGame) {
                 ShowMenu(options);
                 int option = GetOption(1, options.Length);
                 switch (option) {
-                    
-
+                    case 1:
+                        AddUser();
+                        break;
+                    case 2:
+                        ModifyUser();
+                        break;
+                    case 3:
+                        DeleteUser();
+                        break;
+                    case 4:
+                        GetAllUsers();
+                        break;
+                    case 5:
+                        GetLastMatchLog();
+                        break;
+                    case 6:
+                        GetTopScores();
+                        break;
                 }
             }
+        }
+
+
+        public void AddUser()
+        {
+            Console.WriteLine("ADD USER");
+            Console.WriteLine("New user's name");
+            string aNickname =Console.ReadLine();
+            UserDto newUser = new UserDto() { nickname = aNickname };
+            server.AddUser(newUser);
+        }
+
+        private void DeleteUser()
+        {
+            Console.WriteLine("DELETE USER");
+            Console.WriteLine("Please provide the nickname of the user");
+            string aNickname = Console.ReadLine();
+            server.DeleteUser(aNickname);
+        }
+
+        private void GetAllUsers()
+        {
+            Console.WriteLine("REGISTERED USERS");
+            ICollection<UserDto> usersDtos = server.GetAllUsers();
+            foreach (UserDto user in usersDtos) {
+                Console.WriteLine(user.nickname);
+            }
+        }
+
+        private void GetLastMatchLog()
+        {
+            Console.WriteLine("LAST MATCH LOG");
+            string log = server.GetLastMatchLog();
+        }
+
+        private void GetTopScores()
+        {
+            Console.WriteLine("TOP SCORES");
+            ScoreDto[] topScores =server.GetTopScores();
+            int number = 1;
+            foreach (ScoreDto dto in topScores) {
+                Console.Write(number + dto.UserNickname);
+            }
+        }
+
+        private void ModifyUser()
+        {
+            throw new NotImplementedException();
         }
 
         private void ShowMenu(string[] options)
@@ -53,13 +119,6 @@ namespace Client
             }
             return input;
         }
-
-        public void AddUser() {
-            UserDto newUser = new UserDto() { nickname = "Richard", photoPath = "aPhoto" };
-            server.AddUser(newUser);
-        }
-
- 
 
         internal void GetUser()
         {
