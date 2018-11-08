b117a730fde388fb49f56516614e318b520d8cef
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-08 13:26:47 -0300

    se mueve enum a una clase aparte

--- Obligatorio1/ConsoleServer/Program.cs
+++ Obligatorio1/ConsoleServer/Program.cs
@@ -16,15 +16,6 @@ namespace ConsoleServer
         private delegate bool EventHandler(CtrlType sig);
         static EventHandler _handler;
 
-        enum CtrlType
-        {
-            CTRL_C_EVENT = 0,
-            CTRL_BREAK_EVENT = 1,
-            CTRL_CLOSE_EVENT = 2,
-            CTRL_LOGOFF_EVENT = 5,
-            CTRL_SHUTDOWN_EVENT = 6
-        }
-
         public static Server gameServer;
         public static TcpChannel remotingUserStorage;
 
