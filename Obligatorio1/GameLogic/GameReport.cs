742758f5d8d693eaf282dd36b1678bd1c822d5ff
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-09 18:21:17 -0300

    se crea objeto que modela un reporte de la partida, sera utilizado para las estadisticas

---
+++ Obligatorio1/GameLogic/GameReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    public class GameReport
+    {
+        public DateTime Date { get; private set; }
+        public ICollection<PlayerReportField> registers;
+        public GameReport() {
+            Date = DateTime.Now;
+            registers = new List<PlayerReportField>();
+        }
+    }
+}
---
+++ Obligatorio1/GameLogic/PlayerReportField.cs
@@ -0,0 +1,32 @@
+﻿using GameLogic.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    public class PlayerReportField
+    {
+        private string playerName;
+        public string PlayerName { get {return playerName; } private set {SetName(value); } }
+
+        public Role RolePlayed { get; private set; }
+        public bool Won { get; private set; }
+
+        public PlayerReportField(string aName, Role aRole, bool didWin) {
+            PlayerName = aName;
+            RolePlayed = aRole;
+            Won = didWin;
+        }
+
+        private void SetName(string aName)
+        {
+            if (string.IsNullOrWhiteSpace(aName)) {
+                throw new InvalidGameReportException();
+            }
+            playerName = aName;
+        }
+    }
+}
