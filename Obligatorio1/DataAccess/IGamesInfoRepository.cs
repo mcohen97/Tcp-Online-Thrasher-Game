60d9c63fb347ae3ff6b2219270c40c70a58dd3d6
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-09 18:27:15 -0300

    se renombran clases

--- Obligatorio1/DataAccess/IGamesInfoRepository.cs
+++ Obligatorio1/DataAccess/IGamesInfoRepository.cs
@@ -1,16 +1,16 @@
 ﻿using GameLogic;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessInterface
 {
-    public interface IScoreRepository
+    public interface IGamesInfoRepository
     {
         void AddScore(Score aScore);
 
         ICollection<Score> GetScores();
+
+        void AddGameReport(GameReport report);
+
+        ICollection<GameReport> GetGameReports();
     }
 }
