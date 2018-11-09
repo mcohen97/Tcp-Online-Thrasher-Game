f1d8d7e396b885cfcb958d6a06e9d32e25e6a2a3
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-09 18:09:06 -0300

    se crea excepcion

---
+++ Obligatorio1/AdministrativeServer/GameServiceException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AdministrativeServer
+{
+    [Serializable]
+    internal class GameServiceException : Exception
+    {
+        public GameServiceException()
+        {
+        }
+
+        public GameServiceException(string message) : base(message)
+        {
+        }
+
+    }
+}
