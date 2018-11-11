0c1c175d3389bbf159d6d10539037e67bcbad74c
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-11 00:49:15 -0300

    se modifica userService para que guarde la imagen que viene en el dto, y asigne al usuario el path de la foto

--- Obligatorio1/DataAccess/UserService.cs
+++ Obligatorio1/DataAccess/UserService.cs
@@ -12,8 +12,10 @@ namespace DataAccess
     public class UserService : MarshalByRefObject, IUserCRUDService
     {
         private IUserRepository usersStorage;
+        private IImageManager images;
         public UserService() {
             usersStorage = UsersInMemory.instance.Value;
+            images = new ImageManager();
         }
 
         public string AddUser(UserDto userModel)
@@ -21,8 +23,10 @@ namespace DataAccess
             string resultMessage;
             try
             {
-                User toAdd = new User(userModel.nickname, userModel.photoPath);
+                User toAdd = new User(userModel.nickname);
                 usersStorage.AddUser(toAdd);
+                string fullPath =images.SaveImage(userModel.photo, userModel.nickname);
+                toAdd.Path = fullPath;
                 resultMessage = "User was added succesfully";
             }
             catch (UserAlreadyExistsException e)
@@ -52,7 +56,7 @@ namespace DataAccess
         public UserListActionResult GetAllUsers()
         {
             ICollection<User> users = usersStorage.GetAllUsers();
-            IEnumerable<UserDto> dtos = users.Select(u => new UserDto() {nickname=u.Nickname, photoPath =u.Path });
+            IEnumerable<UserDto> dtos = users.Select(u => new UserDto() {nickname=u.Nickname, photo =images.ReadImage(u.Path) });
             UserListActionResult result = new UserListActionResult()
             {
                 Success = true,
@@ -65,7 +69,7 @@ namespace DataAccess
         public UserActionResult GetUser(string nickname)
         {
             User stored = usersStorage.GetUser(nickname);
-            UserDto dto = new UserDto() { nickname = stored.Nickname, photoPath = stored.Path };
+            UserDto dto = new UserDto() { nickname = stored.Nickname, photo = images.ReadImage(stored.Path) };
             UserActionResult result = new UserActionResult()
             {
                 Success = true,
@@ -80,7 +84,9 @@ namespace DataAccess
             string resultMessage;
             try
             {
-                User toModify = new User(newData.nickname, newData.photoPath);
+                User toModify = new User(newData.nickname);
+                string fullPath = images.SaveImage(newData.photo, newData.nickname);
+                toModify.Path = fullPath;
                 usersStorage.ModifyUser(oldNickname, toModify);
                 resultMessage = "User modified succesfully";
             }
