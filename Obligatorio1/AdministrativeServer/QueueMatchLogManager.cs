4de12b09fcb1a113b3a63e2610c54d2df92aa909
Marcel Cohen <diegomarcel27@hotmail.com>
2018-11-15 10:59:32 -0300

    se utiliza arroja la excepcion creada

--- Obligatorio1/AdministrativeServer/QueueMatchLogManager.cs
+++ Obligatorio1/AdministrativeServer/QueueMatchLogManager.cs
@@ -12,17 +12,31 @@ namespace AdministrativeServer
 
         public string GetLastMatchLog()
         {
+            try
+            {
+                return TryGetLog();
+            }
+            catch (InvalidOperationException) {
+                throw new NoLogsAccessException();
+            }
+
+        }
+
+        private string TryGetLog()
+        {
             string log = "No matches were played yet";
             using (MessageQueue logQueue = new MessageQueue(msmqAdress))
             {
                 logQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
 
                 int messagesCount = logQueue.GetAllMessages().Length;
-                while (messagesCount > 1) {
+                while (messagesCount > 1)
+                {
                     logQueue.Receive();
                     messagesCount--;
                 }
-                if (messagesCount == 1) {
+                if (messagesCount == 1)
+                {
                     log = logQueue.Peek().Body.ToString();
                 }
             }
